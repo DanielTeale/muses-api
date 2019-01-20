5649c870dacccbae72b84e6e115d21bdbc38c009
Daniel Teale <djteale90@gmail.com>
2019-01-21 10:55:14 +1100

    added update user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,17 +1,28 @@
 const UserModel = require("../database/models/user_model")
 const JWTService = require("../services/jwt_service")
+const AWSService = require("../services/aws_service")
+const multiparty = require("multiparty")
 
 
-function register(req, res, next) {
-  const { email, password, name, bio } = req.body
-  const user = new UserModel({ email, name })
+async function register(req, res, next) {
+  const { email, password, name, bio, chapter, website } = req.body;
+  // console.log(req)
+  // let form = new multiparty.Form()
+  // form.parse(req, () => {
+    
+  // })
+
+  const user = new UserModel({ email, name, bio, chapter, website })
+  
+  // await AWSService.fileUpload(req, res, next )
 
   UserModel.register(user, password, (err, user) => {
     if (err) {
       return next(console.log(err))
     }
     const token = JWTService.generateToken(user)
-    return res.json(token)
+    const data = {user, token}
+    return res.json(data)
   })
 }
 
@@ -26,10 +37,34 @@ async function loginVerify(req, res, next) {
     return next(res.send("Incorrect name or password"))
   }
   const token = JWTService.generateToken(user)
-  return res.json(token)
+  const data = { user, token }
+  return res.json(data)
+}
+
+async function update(req, res) {
+
+  const {email, name, bio, chapter, website, avatar} = req.body
+  const user = await UserModel.findOne({ email })
+
+
+  try {
+    user.email = email
+    user.name = name
+    user.bio = bio
+    user.chapter = chapter
+    user.website = website
+    user.avatar = avatar
+    await user.save()
+    const token = JWTService.generateToken(user)
+    const data = {user, token}
+    return res.json(data);
+  } catch (err) {
+    return res.status(400).send(err)
+  }
 }
 
 module.exports = {
   register,
-  loginVerify
+  loginVerify,
+  update
 }
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport")
 const {celebrate} = require("celebrate")
 const {userVerification} = require("../services/celebrate_service")
 const UserController = require("../controllers/user_controller")
 
-router.post("/register", celebrate(userVerification), UserController.register);
+// router.post("/register", celebrate(userVerification), UserController.register);
+router.post("/register", UserController.register);
 router.post("/login", celebrate(userVerification), UserController.loginVerify);
+router.patch("/login", passport.authenticate("jwt", { session: false }), UserController.update)
 
 module.exports = router;
