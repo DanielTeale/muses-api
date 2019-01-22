5e94ec115e955c3bfe0ee8633d71b017e45fd7e8
Jessica Alves <jessralves@gmail.com>
2019-01-22 15:42:46 +1100

    Create resources collection

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const SponsorRoutes = require("./sponsor_routes")
 const ChapterRoutes = require("./chapter_routes")
 const passport = require("passport")
 const fileRoute = require("../services/aws_service")
+const ResourceRoutes = require("./resource_routes");
 
 router.get("/", (req, res) => res.send("Welcome"))
 // router.post("/files", fileRoute.fileUpload)
@@ -15,5 +16,6 @@ router.use("/mailinglist", MailingListRoutes)
 router.use("/events", EventRoutes)
 router.use("/sponsor", passport.authenticate("jwt", { session: false }), SponsorRoutes)
 router.use("/chapter", ChapterRoutes);
+router.use("/resources", ResourceRoutes);
 
 module.exports = router;
